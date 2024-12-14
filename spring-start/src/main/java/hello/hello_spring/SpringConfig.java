335517a19444a41c0ea25dd4d3b79a36898f30ff
Woo <gwoprk98@gmail.com>
2024-12-14 22:16:43 +0900

    feat: JPA를 이용한 멤버 리포지토리

--- spring-start/src/main/java/hello/hello_spring/SpringConfig.java
+++ spring-start/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,8 +1,11 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,21 +13,24 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+   private EntityManager em;
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+   @Autowired
+   public SpringConfig(EntityManager em) {
+       this.em = em;
+   }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//      return new MemoryMemberRepository();
-//      return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
-    }
+   @Bean
+   public MemberService memberService() {
+       return new MemberService(memberRepository());
+   }
+
+   @Bean
+   public MemberRepository memberRepository() {
+//     return new MemoryMemberRepository();
+//     return new JdbcMemberRepository(dataSource);
+//     return new JdbcMemberRepository(dataSource);
+       return new JpaMemberRepository(em);
+   }
 }
